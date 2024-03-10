f99717e418031418d9c64a1a6b7be63dbebc697f
mrcolinaa <mate2003zz@gmail.com>
2024-03-10 13:39:30 -0500

    Cambios en la interfaz grafica e importacion del modulo de conexion a la base de datos

--- inventario/inventario.py
+++ inventario/inventario.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
+from PIL import Image, ImageTk
 from inventario.registro import RegistroVentana
 from inventario.actualizacion import ActualizacionVentana
 from inventario.lista import ListaVentana
@@ -18,8 +19,14 @@ class GestionInventario(tk.Toplevel):
         self.create_widgets()
 
     def create_widgets(self):
+        # Icono para el botón de cerrar sesión
+        logout_icon = Image.open("icons/logout.png")
+        logout_icon = self.resize_image(logout_icon, (20, 20))
+        logout_icon = ImageTk.PhotoImage(logout_icon)
+
         # Botón de cerrar sesión arriba a la izquierda
-        logout_button = ttk.Button(self, text="Cerrar Sesión", command=self.logout)
+        logout_button = ttk.Button(self, text="Cerrar Sesión", command=self.logout, image=logout_icon, compound="left")
+        logout_button.image = logout_icon
         logout_button.pack(side="top", padx=10, pady=10, anchor="nw")
 
         # Etiqueta de título
@@ -30,17 +37,64 @@ class GestionInventario(tk.Toplevel):
         button_frame = tk.Frame(self)
         button_frame.pack(pady=20)
 
-        registrar_button = ttk.Button(button_frame, text="Registrar", command=self.abrir_registro)
+        # Establecer un tamaño fijo para los botones
+        button_width = 200
+        button_height = 50
+
+        # Agregar iconos a los botones y ajustar su tamaño manteniendo la proporción
+        icon_size = (button_width - 20, button_height - 20)
+
+        # Botón Registrar
+        registrar_icon = Image.open("icons/register.png")
+        registrar_icon = self.resize_image(registrar_icon, icon_size)
+        registrar_icon = ImageTk.PhotoImage(registrar_icon)
+        registrar_button = ttk.Button(button_frame, text="Registrar", command=self.abrir_registro, image=registrar_icon, compound="left")
+        registrar_button.image = registrar_icon
+        registrar_button.config(width=button_width)
         registrar_button.pack(side="top", padx=10, pady=10, fill="x")
 
-        actualizar_button = ttk.Button(button_frame, text="Actualizar", command=self.abrir_actualizacion)
+        # Botón Actualizar
+        actualizar_icon = Image.open("icons/update.png")
+        actualizar_icon = self.resize_image(actualizar_icon, icon_size)
+        actualizar_icon = ImageTk.PhotoImage(actualizar_icon)
+        actualizar_button = ttk.Button(button_frame, text="Actualizar", command=self.abrir_actualizacion, image=actualizar_icon, compound="left")
+        actualizar_button.image = actualizar_icon
+        actualizar_button.config(width=button_width)
         actualizar_button.pack(side="top", padx=10, pady=10, fill="x")
 
-        listar_button = ttk.Button(button_frame, text="Listar", command=self.abrir_lista)
+        # Botón Listar
+        listar_icon = Image.open("icons/list.png")
+        listar_icon = self.resize_image(listar_icon, icon_size)
+        listar_icon = ImageTk.PhotoImage(listar_icon)
+        listar_button = ttk.Button(button_frame, text="Listar", command=self.abrir_lista, image=listar_icon, compound="left")
+        listar_button.image = listar_icon
+        listar_button.config(width=button_width)
         listar_button.pack(side="top", padx=10, pady=10, fill="x")
 
-        seguimiento_button = ttk.Button(button_frame, text="Seguimiento", command=self.abrir_seguimiento)
+        # Botón Seguimiento
+        seguimiento_icon = Image.open("icons/track.png")
+        seguimiento_icon = self.resize_image(seguimiento_icon, icon_size)
+        seguimiento_icon = ImageTk.PhotoImage(seguimiento_icon)
+        seguimiento_button = ttk.Button(button_frame, text="Seguimiento", command=self.abrir_seguimiento, image=seguimiento_icon, compound="left")
+        seguimiento_button.image = seguimiento_icon
+        seguimiento_button.config(width=button_width)
         seguimiento_button.pack(side="top", padx=10, pady=10, fill="x")
+
+    def resize_image(self, image, size):
+        """Resize an image while maintaining aspect ratio."""
+        original_width, original_height = image.size
+        target_width, target_height = size
+        aspect_ratio = original_width / original_height
+
+        if target_width / aspect_ratio <= target_height:
+            new_width = int(target_width)
+            new_height = int(target_width / aspect_ratio)
+        else:
+            new_width = int(target_height * aspect_ratio)
+            new_height = int(target_height)
+
+        resized_image = image.resize((new_width, new_height), Image.NEAREST)
+        return resized_image
 
     def logout(self):
         self.destroy()
--- inventario/registro.py
+++ inventario/registro.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
+import re
+from conexionDB import database
 
 class RegistroVentana(tk.Toplevel):
     def __init__(self, parent):
@@ -39,8 +41,10 @@ class RegistroVentana(tk.Toplevel):
         label_quantity = ttk.Label(card_frame, text="Cantidad:", font=("Arial", 10))
         label_quantity.grid(row=5, column=0, sticky="w", padx=10)
 
-        self.quantity_entry = ttk.Entry(card_frame, font=("Arial", 10))
+        self.quantity_var = tk.StringVar()
+        self.quantity_entry = ttk.Entry(card_frame, textvariable=self.quantity_var, font=("Arial", 10))
         self.quantity_entry.grid(row=5, column=1, padx=10, pady=5, sticky="we")
+        self.quantity_entry.configure(validate="key", validatecommand=(self.register(self.validate_quantity), "%P"))
 
         label_category = ttk.Label(card_frame, text="Categoria:", font=("Arial", 10))
         label_category.grid(row=6, column=0, sticky="w", padx=10)
@@ -59,6 +63,8 @@ class RegistroVentana(tk.Toplevel):
         cancel_button = ttk.Button(button_frame, text="Cancelar", command=self.on_close, style="Cancel.TButton")
         cancel_button.grid(row=0, column=1, padx=10, sticky="we")
 
+    def validate_quantity(self, new_value):
+        return re.match(r"^\d*$", new_value) is not None
 
     def registrar_elemento(self):
         name = self.name_entry.get()
@@ -66,8 +72,36 @@ class RegistroVentana(tk.Toplevel):
         quantity = self.quantity_entry.get()
         category = self.category_combobox.get()
 
-        # Aquí puedes implementar la funcionalidad para registrar un elemento en el inventario
-        messagebox.showinfo("Registrar", f"Registrar elemento:\nNombre: {name}\nCódigo: {code}\nCantidad: {quantity}\nCategoría: {category}")
+        # Verifica si todos los campos están completos
+        if not name or not code or not quantity or not category:
+            messagebox.showerror("Error", "Por favor complete todos los campos.")
+            return
+
+        connection = database.connect_to_database()
+        if connection:
+            try:
+                cursor = connection.cursor()
+
+                # Aquí ejecutas la inserción en la base de datos
+                cursor.execute("INSERT INTO inventario (nombre, codigo, cantidad, categoria) VALUES (%s, %s, %s, %s)", (name, code, quantity, category))
+
+                connection.commit()
+
+                messagebox.showinfo("Registrar", "Registro exitoso!")
+
+                # Vaciar los campos después del registro exitoso
+                self.name_entry.delete(0, tk.END)
+                self.code_entry.delete(0, tk.END)
+                self.quantity_var.set("")
+                self.category_combobox.set("Seleccione la categoria")
+            except Exception as e:
+                print("Error al registrar elemento:", e)
+                messagebox.showerror("Error", "Error al registrar elemento.")
+            finally:
+                cursor.close()
+                connection.close()
+        else:
+            print("Error: No se pudo conectar a la base de datos.")
 
     def on_close(self):
         if messagebox.askokcancel("Cancelar", "¿Estás seguro de que quieres cancelar el registro?"):
